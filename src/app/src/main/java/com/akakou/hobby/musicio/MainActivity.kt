263eac2a1d35a9143c274f081911124e0c2f162d
akakou <s16003@tokyo.kosen-ac.jp>
2018-06-21 17:56:35 +0900

    [add] Play Youtube

--- src/app/src/main/java/com/akakou/hobby/musicio/MainActivity.kt
+++ src/app/src/main/java/com/akakou/hobby/musicio/MainActivity.kt
@@ -2,11 +2,46 @@ package com.akakou.hobby.musicio
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.*
+import java.security.Provider
+import com.google.android.youtube.player.YouTubePlayerView
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeInitializationResult
 
-class MainActivity : AppCompatActivity() {
+
+/*
+* メインアクティビティ
+*
+* Youtubeの再生も行う
+* */
+class MainActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    private val API_TOKEN = ""
+    private var videoId = ""
+    private var youTubeView: YouTubePlayerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Youtube Viewの初期化
+        youTubeView = findViewById(R.id.youtube_view) as YouTubePlayerView;
+        youTubeView!!.initialize(API_TOKEN, this);
+    }
+
+    /**
+     * Youtube Viewの初期化が成功した場合に呼ばれる
+     */
+    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
+        if (!wasRestored) {
+            player!!.loadVideo(videoId)
+        }
+    }
+
+    /**
+     * Youtube Viewの初期化が成功した場合に呼ばれる
+     */
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, errorReason: YouTubeInitializationResult?) {
+        val errorMessage = String.format("ERR", errorReason.toString())
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
     }
 }
