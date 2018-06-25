726250076df312bc30b336757dee9cb2e1357927
akakou <s16003@tokyo.kosen-ac.jp>
2018-06-25 18:02:33 +0900

    [add] Make playId To List

--- src/app/src/main/java/com/akakou/hobby/musicio/YoutubeActivity.kt
+++ src/app/src/main/java/com/akakou/hobby/musicio/YoutubeActivity.kt
@@ -11,14 +11,17 @@ import com.google.android.youtube.player.YouTubePlayerView
 
 import kotlinx.android.synthetic.main.activity_youtube.*
 
+
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
     private val API_TOKEN = "AIzaSyCeWrJL5RurjqZz1f-okFt0JvATeYvkPhA"
-    private var videoId = "uwph0dv9E6U"
+    val videList: MutableList<String> = mutableListOf()
     private var youTubeView: YouTubePlayerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_youtube);
+        setContentView(R.layout.activity_youtube);
+
+        videList.add("uwph0dv9E6U")
 
         // Youtube Viewの初期化
         youTubeView = findViewById(R.id.youtube_view) as YouTubePlayerView;
@@ -30,7 +33,7 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
      */
     override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
         if (!wasRestored) {
-            player!!.loadVideo(videoId)
+            player!!.loadVideo(videList[0])
         }
     }
 
